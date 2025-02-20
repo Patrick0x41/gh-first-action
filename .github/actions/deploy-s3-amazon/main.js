2bad009121517a75166f3c027101f8fc3a11f857
Patrick0x41 <93409645+Patrick0x41@users.noreply.github.com>
2025-02-20 16:16:51 +0200

    a

--- .github/actions/deploy-s3-amazon/main.js
+++ .github/actions/deploy-s3-amazon/main.js
@@ -17,6 +17,10 @@ function run()
     
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
     
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url',websiteUrl)  ; // equilivnet to ::set-output
+
+
     core.notice('Hello from my custom javascript action!');
 }
 
